d14558bc93f1aa8dcf33a484ade04c059e34812d
Bashir Galeev <bashir.2002@mail.ru>
2021-04-10 16:11:52 +0300

    Added Splay-Tree features

---
+++ cmake-build-debug/CMakeFiles/Splay_Trees_Semestrovka.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Splay_Trees_Semestrovka.dir/Splay_Tree.cpp.obj"
+  "Splay_Trees_Semestrovka.exe"
+  "Splay_Trees_Semestrovka.exe.manifest"
+  "Splay_Trees_Semestrovka.pdb"
+  "libSplay_Trees_Semestrovka.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Splay_Trees_Semestrovka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
